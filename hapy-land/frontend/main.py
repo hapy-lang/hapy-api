521b78cb411f18276160a8b0e546ceb11b5d3ad3
LeanKhan <eslean20@gmail.com>
2021-10-29 22:10:28 +0100

    rename backend -> frontend

--- hapy-land/frontend/main.py
+++ hapy-land/frontend/main.py
@@ -3,30 +3,30 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-backend = FastAPI()
+frontend = FastAPI()
 
-backend.mount("/static", StaticFiles(directory="backend/static"), name="static")
+frontend.mount("/static", StaticFiles(directory="frontend/static"), name="static")
 
 
-templates = Jinja2Templates(directory="backend/templates")
+templates = Jinja2Templates(directory="frontend/templates")
 
-@backend.get("/", response_class=HTMLResponse)
+@frontend.get("/", response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@backend.get("/examples", response_class=HTMLResponse)
+@frontend.get("/examples", response_class=HTMLResponse)
 def examples(request: Request):
     return templates.TemplateResponse("examples.html", {"request": request})
 
-@backend.get("/examples/{id}", response_class=HTMLResponse)
+@frontend.get("/examples/{id}", response_class=HTMLResponse)
 def example(request: Request, id: str):
     return templates.TemplateResponse("example.html", {"request": request, "id": id})
 
-@backend.get("/bites", response_class=HTMLResponse)
+@frontend.get("/bites", response_class=HTMLResponse)
 def bites(request: Request):
     return templates.TemplateResponse("bites.html", {"request": request, "id": id})
 
-@backend.get("/bites/{id}", response_class=HTMLResponse)
+@frontend.get("/bites/{id}", response_class=HTMLResponse)
 def bite(request: Request, id: str):
     return templates.TemplateResponse("bite.html", {"request": request, "id": id})
 
