462ab228d87bedf740521e6984a4ad386a5b502c
Shugaba-Wuta <57352948+Shugaba-Wuta@users.noreply.github.com>
2021-11-03 22:20:20 +0100

    run endpoint and data models for ORM

--- hapy-land/api/main.py
+++ hapy-land/api/main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from datetime import datetime
+from fastapi import FastAPI, status, HTTPException, Response, File, UploadFile, Form
+from sqlalchemy.orm import Session
+from typing import Optional
+
+# Hapy imports
 from hapy.transpiler import transpile
 from hapy.exector import run as run_python
 
-from .models import Request
+
+# Local  imports aka imports from the lib.
+from .database import SessionLocal
+from .models import BiteBase, Challenge, User as UserModel
+from .pydantic_models import Request, User, RequestResponse
 
 
 api = FastAPI()
@@ -12,37 +21,115 @@ api = FastAPI()
 # Update
 # Delete CRUD
 
-
-@api.get("/")
-def index():
-    return {"data": "Index"}
+db = SessionLocal()
 
 
-@api.post("/run")
+@api.get("/challenges")
+def index(limit: int = 10):
+    # Fetch all bites from the data store.
+    fetch_all = db.query(Challenge).limit(limit).all()
+    return {"data": fetch_all}
+
+
+@api.post("/register", status_code=status.HTTP_201_CREATED)
+def register_user(new_user: User):
+    """A Post request to send new user data and write it to a data store."""
+    username = new_user.username
+    timestamp = datetime.now()
+    email = new_user.email
+    # Hash the password
+    password = new_user.password
+    register = UserModel(
+        username=username, timestamp=timestamp, password=password, email=email
+    )
+
+    # Verify That this user does not exist
+    db_user = (
+        db.query(UserModel).filter(UserModel.username == new_user.username).count()
+    )
+    if db_user > 0:
+        raise HTTPException(status_code=400, detail="User already exists")
+
+    db.add(register)
+    db.commit()
+    db.refresh(register)
+    # return register
+    return {
+        "data": {"username": new_user.username},
+        "status": "success",
+        "message": "Successfully created new user.",
+    }
+
+
+@api.post("/login/", response_model=RequestResponse)
+def login_user(user: User, response: Response):
+    """Authenticate user by sending user credentials"""
+    get_user = db.query(UserModel).filter(UserModel.username == user.username)
+    if get_user.count() == 1:
+        # Set Cookie
+        response.set_cookie(key=get_user.first().username, value="random")
+        print(f"\n\n\n\n{response}\n\n\n\n")
+        return {
+            "data": {"user": get_user.first().username},
+            "message": "Successfully returned user",
+            "status": "success",
+        }
+    elif get_user.count() > 1:
+        # This should not be the case. However, get ready yo log an issue
+        pass
+    else:
+        return {
+            "data": {"user": None},
+            "message": "User does not exist",
+            "status": "failed",
+        }
+
+
+def execute(code, option):
+    if code:
+        code_in_python = transpile(code)
+        result = run_python(code_in_python, return_output=True, cloud=True)
+        result = result[1] if result[1] else result[0]
+        translated = (
+            code_in_python
+            if option in ("translate_only", "translate_and_execute")
+            else None
+        )
+
+        python_result = (
+            result if option in ("execute_only", "translate_and_execute") else None
+        )
+        return {
+            "data": {
+                "python_result": python_result,
+                "python_source": translated,
+            },
+            "status": "success" if (python_result or translated) else "error",
+            "message": "Hapy output",
+        }
+    return {
+        "data": {"python_result": None, "python_source": None},
+        "status": "error",
+        "message": "Invalid code/ or file",
+    }
+
+
+@api.post("/run", response_model=RequestResponse, status_code=status.HTTP_200_OK)
 def run(item: Request):
     """Execute code, transpile code, execute and transpile"""
-    if item.code != None:
-        translated = transpile(item.code)
-        if item.translate and not item.execute_only:
-            """This returns python source code and a result from python_source execution"""
-            executed = run_python(translated, cloud=False, return_output=True)
-            # executed returns a tuple of (error, result)
-            python_result = executed[1] if executed[0] == "" else executed[0]
-            return {
-                "data": {"python_result": python_result, "python_source": translated},
-                "status": "success",
-                "message": "Python equivalence of Hapy Code + Result of Hapy execution",
-            }
-        elif item.execute_only:
-            # This checks if the user wants just a python_source
-            return {
-                "data": {"python_source": translated},
-                "status": "success",
-                "message": "Python equivalence of Hapy code",
-            }
-        else:
-            return {
-                "data": None,
-                "status": "error",
-                "message": "translate and execute_only cannot be both false",
-            }
+    if item.code:
+        return execute(item.code, item.option)
+    else:
+        return {
+            "data": {"python_result": None, "python_source": None},
+            "status": "error",
+            "message": "Invalid code/ or file",
+        }
+
+
+@api.post("/run_file/")
+async def create_upload_file(file: UploadFile = File(...), option: str = Form(...)):
+
+    content = await file.read()
+    content = content.decode("utf-8")
+    return execute(content, option=option)
--- hapy-land/api/models.py
+++ hapy-land/api/models.py
@@ -1,9 +1,62 @@
-from pydantic import BaseModel
-from typing import Optional
+from sqlalchemy import (
+    Boolean,
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    TIMESTAMP,
+    Text,
+)
+from sqlalchemy.orm import relationship
+
+from .database import Base
 
 
-class Request(BaseModel):
-    code: str
-    translate: Optional[bool]
-    save: Optional[bool]
-    execute_only: Optional[bool] = False
+# DB Models
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(25), nullable=False, unique=True)
+    timestamp = Column(TIMESTAMP, nullable=False)
+    password = Column(String(25), nullable=False)
+    email = Column(Text, nullable=False)
+
+    # Relationships
+    solutions = relationship("BiteBase", back_populates="uploaded_by")
+
+    def __repr__(self) -> str:
+        return f"<class User>id: {self.id}, username: {self.username}, timestamp: {self.timestamp} "
+
+
+class UserLogin(Base):
+    __tablename__ = "login"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user = Column(Integer, ForeignKey("user.id"))
+    user_agent = Column(Text)
+    mac_address = Column(Text)
+    ip_address = Column(Text)
+    timestamp = Column(TIMESTAMP, nullable=False)
+
+
+class BiteBase(Base):
+    __tablename__ = "bite"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    code = Column(Text(), nullable=False)
+    upload_timestamp = Column(TIMESTAMP)
+    submitter_id = Column(Integer, ForeignKey("user.id"))
+    # relationship
+    uploaded_by = relationship("User", back_populates="solutions")
+
+
+class Answer(BiteBase):
+    challenge_id = Column(Integer, ForeignKey("challenge.id"))
+    challenge = relationship("Challenge", back_populates="submitted_answers")
+
+
+class Challenge(Base):
+    __tablename__ = "challenge"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(255), nullable=False)
+    content = Column(Text, nullable=False)
+    upload_timestamp = Column(TIMESTAMP)
+    submitted_answers = relationship("Answer", back_populates="challenge")
