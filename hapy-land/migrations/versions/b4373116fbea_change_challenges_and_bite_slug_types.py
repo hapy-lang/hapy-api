2ae28fc1b959bbbfd52998f260620b1477e14370
LeanKhan <eslean20@gmail.com>
2021-11-12 20:15:12 +0100

    add alembic

---
+++ hapy-land/migrations/versions/b4373116fbea_change_challenges_and_bite_slug_types.py
@@ -0,0 +1,24 @@
+"""change Challenges and Bite slug types
+
+Revision ID: b4373116fbea
+Revises: 6bf0f8533a82
+Create Date: 2021-11-12 16:23:32.832163
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b4373116fbea'
+down_revision = '6bf0f8533a82'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
