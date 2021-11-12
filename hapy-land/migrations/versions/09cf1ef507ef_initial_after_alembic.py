85801a50ed1cd7d6ce70d4a14973656147e2c085
LeanKhan <eslean20@gmail.com>
2021-11-12 20:15:12 +0100

    add alembic

---
+++ hapy-land/migrations/versions/09cf1ef507ef_initial_after_alembic.py
@@ -0,0 +1,24 @@
+"""initial after alembic
+
+Revision ID: 09cf1ef507ef
+Revises: 
+Create Date: 2021-11-12 16:03:29.640228
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '09cf1ef507ef'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
