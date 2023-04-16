b260368ed5706f62ed211bef47b5d83f636476f1
Emmanuel Segun-Lean <eslean20@gmail.com>
2023-04-16 23:41:23 +0100

    test

--- app/main.py
+++ app/main.py
@@ -4,18 +4,6 @@ from app.api.main import api
 
 app = FastAPI()
 
-origins = [
-    "*"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 # SUB APPS
 # api
 # frontend
