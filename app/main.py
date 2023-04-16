cda55b0ff00da3bc8bb1d74cc11014b1c4894359
Emmanuel Segun-Lean <eslean20@gmail.com>
2023-04-16 23:53:52 +0100

    revert cors

--- app/main.py
+++ app/main.py
@@ -4,6 +4,18 @@ from app.api.main import api
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # SUB APPS
 # api
 # frontend
